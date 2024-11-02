e8f86a417c141ac3510125edeb6575b9630643b9
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-11-02 21:34:33 +0300

    Add files via upload

---
+++ Введение в потоки.py
@@ -0,0 +1,44 @@
+from time import sleep, perf_counter
+from threading import Thread
+
+# Функция записи в файл
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for i in range(1, word_count + 1):
+            f.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+# Измерение времени для последовательного выполнения
+start_time = perf_counter()
+
+# Последовательный вызов функции с разными аргументами
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+end_time = perf_counter()
+print(f"Работа функций {end_time - start_time:.6f} секунд")
+
+# Измерение времени для параллельного выполнения с потоками
+start_time = perf_counter()
+
+# Создание потоков для параллельного выполнения
+threads = [
+    Thread(target=write_words, args=(10, 'example5.txt')),
+    Thread(target=write_words, args=(30, 'example6.txt')),
+    Thread(target=write_words, args=(200, 'example7.txt')),
+    Thread(target=write_words, args=(100, 'example8.txt'))
+]
+
+# Запуск потоков
+for thread in threads:
+    thread.start()
+
+# Ожидание завершения потоков
+for thread in threads:
+    thread.join()
+
+end_time = perf_counter()
+print(f"Работа потоков {end_time - start_time:.6f} секунд")
